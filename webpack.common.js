9d0725eae187cc59d36d54c0fbc1dcdae77447bc
haha <390519764@qq.com>
2018-08-16 23:47:10 +0800

    create a webpack template

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HmtlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: '[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HmtlWebpackPlugin({
+            template: 'index.html',
+            inject: 'head'
+        }),
+        new CleanWebpackPlugin('dist')
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use:[
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+}
