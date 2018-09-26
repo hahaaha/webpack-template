d7b6713200da3b7aa3139bd673c1d8432df5c2b5
unknown <LIUFEIXIAO@corp.bl.com>
2018-09-26 09:47:45 +0800

    use babel,and use browserslist

--- webpack.common.js
+++ webpack.common.js
@@ -34,6 +34,11 @@ module.exports = {
                 use:[
                     'file-loader'
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
             }
         ]
     }
